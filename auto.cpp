b1c60296f948edecda2ce894523f3df58fa27848
Parth Garg <parthgarg750@gmail.com>
2025-01-30 22:48:17 +0530

    Basic learnings

---
+++ auto.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    auto x = 8;
+    auto y = 3.14;
+    auto z = "parth";
+
+    cout << x << " " << y << " " << z << endl;
+
+    // auto is used to automatically declare the type of variable
+    //auto y = 'h'; this is going to be a error because the y is already declared as a double
+
+
+}
